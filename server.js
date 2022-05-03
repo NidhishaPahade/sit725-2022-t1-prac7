516b2a9667d8b07b2851df7a44ee7659f0bfac05
NidhishaPahade <101486006+NidhishaPahade@users.noreply.github.com>
2022-05-04 08:56:00 +1000

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,10 @@ let io = require('socket.io')(http);
 // routes
 let projectsRoute = require('./routes/projects')
 
-
 var port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use('/api/projects',projectsRoute)
-
 
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
@@ -34,17 +32,16 @@ app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
 })
 
 //socket test
+let io = require('socket.io')(http);
 io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
+  console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
   setInterval(()=>{
     socket.emit('number', parseInt(Math.random()*10));
   }, 1000);
-
 });
-
 
 http.listen(port,()=>{
   console.log("Listening on port ", port);
